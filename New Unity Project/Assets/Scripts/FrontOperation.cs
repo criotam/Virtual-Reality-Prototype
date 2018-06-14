bed5cece4dc1c8f76cda9f0c21788668bcf7ca8b
pratikbanjare <jay.lelouch@outlook.com>
2018-06-14 14:18:21 +0530

    Player, Model, front peration
    I have creted the player, added the model, material,. THe turn off/on operation is partially completed. I will link all animations a while later.

---
+++ New Unity Project/Assets/Scripts/FrontOperation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontOperation : MonoBehaviour {
+
+    [SerializeField]
+    Material BaseMaterial, SecondaryMaterial;
+
+
+    public void OnPointerEnter() {
+        GetComponent<Renderer>().material = SecondaryMaterial;
+    }
+
+    public void OnPointerExit() {
+        GetComponent<Renderer>().material = BaseMaterial;
+    }
+
+    public void OnPointerClick() {
+
+    }
+
+}
