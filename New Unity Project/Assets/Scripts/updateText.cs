a1e5f5d0bc609452c265e6367cc84d2e6e3727d3
shreyasa <theshreyasaxena@gmail.com>
2018-06-18 00:19:11 +0530

    UI and Script for updatinng Sheet Thickness Added

---
+++ New Unity Project/Assets/Scripts/updateText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updateText : MonoBehaviour
+{
+
+    //Text Field to target
+    public Text textShow = null;
+
+	private void Start()
+	{
+        Debug.Log(textShow.text);
+	}
+	public void changeText(string toChangeValue)
+    {
+        textShow.text = textShow.text + toChangeValue;
+
+        Debug.Log(textShow.text);
+    }
+}
